eb03b6186fc4bd60afacf1d9ff106826bb47d8f5
David Brendel <mantus667@googlemail.com>
2015-05-13 22:26:52 +0200

    Grid Renderer
    finish custom renderer for editor

--- GridMaps/StartUp.cs
+++ GridMaps/StartUp.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Umbraco.Core;
+using Umbraco.Core.Logging;
 
 namespace GridMaps
 {
-    public class StartUp
+    public class StartUp : ApplicationEventHandler
     {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            try
+            {
+                var src = HttpContext.Current.Server.MapPath("~/App_Plugins/GridMaps/Config/editor.json");
+                var trg = HttpContext.Current.Server.MapPath("~/config/grid.editors.config.js");
+
+                if (File.Exists(src) && File.Exists(trg))
+                {
+                    var srcJson = File.ReadAllText(src);
+                    var trgJson = File.ReadAllText(trg);
+
+                    if (!string.IsNullOrWhiteSpace(srcJson))
+                    {
+                        var srcObj = JsonConvert.DeserializeObject<JObject>(srcJson);
+                        var trgArr = JsonConvert.DeserializeObject(trgJson) as JArray ?? new JArray();
+
+                        if (srcObj != null)
+                        {
+                            if (!trgArr.Any(x => x["alias"].Value<string>() == srcObj["alias"].Value<string>()))
+                            {
+                                trgArr.Add(srcObj);
+                                File.WriteAllText(trg, JsonConvert.SerializeObject(trgArr, Formatting.Indented), Encoding.UTF8);
+                                LogHelper.Info<StartUp>("[GridMaps] Done editing grid editor config.");
+                            }                            
+                        }
+                    }
+                }
+                LogHelper.Info<StartUp>("[GridMaps] Editor file not found.");
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error<StartUp>("[GridMaps] Error merging grid editor config.", ex);
+            }
+            base.ApplicationStarted(umbracoApplication, applicationContext);
+        }
     }
 }
